6ef687df311460e04dfecf3e25d3d6ed1aa4e2e1
Laurigit <irualgm@gmail.com>
2020-03-25 22:23:21 +0200

    drawing map in a good shape

--- scripts/tab/tab_game_status.R
+++ scripts/tab/tab_game_status.R
@@ -35,46 +35,35 @@ output$game_map <- renderPlot({
 
   #CYCLER_TYPE, PIECE_ATTRIBUTE, X = LANE, Y = GAME_SLOT_ID, Z = TEAM_COLOR
   ADM_CYCLER_INFO
-  cycler_pos <- game_status()[CYCLER_ID > 0, .(LANE_graph = 4- LANE_NO, GAME_SLOT_ID, PIECE_ATTRIBUTE, CYCLER_ID)]
+  cycler_pos <- game_status()[, .(LANE_graph = 4- LANE_NO, GAME_SLOT_ID, PIECE_ATTRIBUTE, CYCLER_ID, LANE_NO)]
   my_cycler <- 1
   my_slot <- cycler_pos[CYCLER_ID == my_cycler, GAME_SLOT_ID]
-  cycler_pos[, SLOT_Y_AXIS := GAME_SLOT_ID - my_slot + 1]
+  cycler_pos[, SLOT_Y_AXIS := GAME_SLOT_ID - my_slot + 0.5]
   cyc_type <- ADM_CYCLER_INFO[, .(SHORT_TYPE, CYCLER_ID)]
   joinaa <- cyc_type[cycler_pos, on = "CYCLER_ID"]
 
 
-  scale_fill_rider_background <- function() {
-   manual_scale(
-      aesthetics = 'taustavari',
-      value = setNames(c("red", "blue", "black", "green", "pink", "white"),
-                       c(1, 2, 3, 4, 5, 6))
-    )
-  }
 
-  colors <- c("red", "blue", "black", "green", "pink", "white", "brown", "deepskyblue1", "grey")
-  color_map <- c("1" = "red",
-                "2" = "blue",
-                "3" = "black",
-                "4" ="green",
-                "5" = "pink",
-                "6" = "white",
-                "7" = "brown",
-                "8" = "deepskyblue1",
-                "9" = "grey")
-  color_mapping_table <- data.table(CYCLER_ID = c(1,2,3,4,5,6,7,8,9,10,11,12),
-                                    color_id = c(1,1,2,2,3,3,4,4,5,5,6,6),
-                                    font_color = c(6,6,6,6,6,6,6,6,3,3,3,3))
+  color_mapping_table <- data.table(CYCLER_ID = c(0,1,2,3,4,5,6,7,8,9,10,11,12),
+                                    color_id = c(9,1,1,2,2,3,3,4,4,5,5,6,6),
+                                    font_color = c(9,6,6,6,6,6,6,6,6,3,3,3,3))
   piece_attribute_map <- data.table(PIECE_ATTRIBUTE  = c("N", "M", "A", "C", "S"),
-                                    pa_color = c(9, 1, 2, 7, 8))
+                                    pa_color = c(6, 1, 2, 7, 8))
 
 
   join_colors <- color_mapping_table[joinaa, on = "CYCLER_ID"]
 join_pa_map <- piece_attribute_map[join_colors, on = "PIECE_ATTRIBUTE"]
 print(join_pa_map)
-  p1 <- ggplot(join_pa_map,
+#dont shoe more lanes than needed
+
+finish_slot <- game_status()[FINISH == 1, max(GAME_SLOT_ID)]
+max_lanes <- game_status()[GAME_SLOT_ID <= finish_slot, max(LANE_NO)]
+filter_lanes <- join_pa_map[LANE_NO <= max_lanes]
+
+  p1 <- ggplot(filter_lanes,
              aes(x = LANE_graph, y = SLOT_Y_AXIS, fill = factor(color_id))) +
   #geom_tile(color = "gray", size = 5) +
-  geom_tile(aes( color=as.factor(pa_color), width=0.9, height=0.9), size=2) +
+  geom_tile(aes( color=as.factor(pa_color), width = 1, height = 1), size=2) +
   geom_text(aes(label = SHORT_TYPE, color = as.factor(font_color)), size = 10) +
     scale_fill_manual(values=c("1" = "red",
                                "2" = "blue",
@@ -93,7 +82,21 @@ print(join_pa_map)
                                 "6" = "white",
                                 "7" = "brown",
                                 "8" = "deepskyblue1",
-                                "9" = "grey"))
+                                "9" = "grey"))+
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "none"
+) +
+    scale_x_continuous(limits = c(3.5 - max_lanes ,3.5), expand = c(0, 0)) +
+
+   scale_y_continuous(limits = c(0,10), expand = c(0, 0), breaks = c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5),
+                      label = c(0, 1,2,3, 4, 5, 6, 7, 8, 9))
+ #
+  #    scale_x_continuous(limits = c(0,3), expand = c(0, 0)) +
+  # +
+  #   scale_y_discrete(
+  #                    limits=c("1","2","3","4","5","6", "7", "8", "9"))
     # scale_colour_manual(
     #   values = color_map,
     #   aesthetics = c("colour", "fill")
