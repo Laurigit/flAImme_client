067944d184626a8d24c3451cb083153f2a4618d0
Laurigit <irualgm@gmail.com>
2020-04-04 17:53:42 +0300

    mobile testing

--- scripts/ui/ui_human_input.R
+++ scripts/ui/ui_human_input.R
@@ -1,9 +1,9 @@
 tabItem(tabName = "tab_human_input",
         fluidPage(
           fluidRow(div(DT::dataTableOutput(outputId = "players")), style = "50%"),
-          fluidRow(splitLayout(div(DT::dataTableOutput(outputId = "sprinter_deck"), style = "font-size: 90%; width: 55%"),
-                    div(DT::dataTableOutput(outputId = "rouler_deck"), style = "font-size: 90%; width: 55%"))),
-          fluidRow(div(DT::dataTableOutput("other_decks"), style = "font-size: 80%; width: 100%")),
+          fluidRow(splitLayout(div(DT::dataTableOutput(outputId = "sprinter_deck"), style = "font-size: 80%; width: 50%"),
+                    div(DT::dataTableOutput(outputId = "rouler_deck"), style = "font-size: 80%; width: 50%"))),
+          fluidRow(div(DT::dataTableOutput("other_decks"), style = "font-size: 70%; width: 90%")),
           fluidRow(actionBttn(inputId = "show_map", label = "Show map", style = "material-flat", color = "primary", size = "lg"))
 
 
